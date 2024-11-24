8b2cfbfe7d1ef6b247bf42b97ea9959dab18b971
GuiDecker <guilherme.deck1227@gmail.com>
2024-11-24 15:57:04 -0300

    react query setup

--- src/app.tsx
+++ src/app.tsx
@@ -3,40 +3,18 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDate: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: Date
-    }[]
-  >
-}
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null | undefined>(
-    null
-  )
-  useEffect(() => {
-    const fetchSummaryCompleatedGoals = async () => {
-      try {
-        const { data } = await axios.get('http://localhost:3333/summary')
-        setSummary(data.summary)
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
-    fetchSummaryCompleatedGoals()
-  }, [])
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
   return (
     <Dialog>
       <EmptyGoals />
-      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
